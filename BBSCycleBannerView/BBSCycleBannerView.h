4ae04720d80172c1d5dcbfd5e47f346810c3f525
fuhang <fuhang@farmfriend.cn>
2018-12-14 19:11:34 +0800

    轮播图

---
+++ BBSCycleBannerView/BBSCycleBannerView.h
@@ -0,0 +1,17 @@
+//
+//  BBSCycleBannerView.h
+//  BBSCycleBannerView
+//
+//  Created by 付航 on 2018/12/13.
+//  Copyright © 2018年 BBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BBSCycleBannerView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BBSCycleBannerView/BBSCycleBannerView.m
@@ -0,0 +1,21 @@
+//
+//  BBSCycleBannerView.m
+//  BBSCycleBannerView
+//
+//  Created by 付航 on 2018/12/13.
+//  Copyright © 2018年 BBS. All rights reserved.
+//
+
+#import "BBSCycleBannerView.h"
+
+@implementation BBSCycleBannerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
